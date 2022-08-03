dedc7a6b3a53675d496a90a1fb565a76854fd2a8
artem subbotin <subbota1991@gmail.com>
2022-08-03 19:48:28 +0400

    init: start task morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,12 +35,44 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    "**********": " ",
 };
-
-function decode(expr) {
-    // write your solution here
+const dotDash={
+    10: ".",
+    11: "-",
+    "**": "**",
+    00: "00",
+ 
 }
+let a=[]
+let b=[]
+let c=[]
+let g=0
+let h=[]
+let j=[]
+function decode(expr) {
+    let words=""
+    for(let i=0;i<expr.length;){
+        a=expr.slice(i,i+10)
+        i=i+10
+        for(let i=0;i<10;){
+            b=a.slice(i,i+2)
+            c=c.concat(b)
+            h=h.concat(dotDash[(c[g])])
+            if(h.length==5){
+                j=j.concat(h.join(""))
+                words=words.concat(MORSE_TABLE[(j[0])])
+                h=[]
+                j=[]
+            }
+        i=i+2
+        g=g+1
+        }
+       
+    }
+return words
 
+}
 module.exports = {
     decode
 }
